e1e73d23fdabe1a56a25d934371691f6b9ba5ea0
Ankan-Basu <an.basu.kan@gmail.com>
2024-05-13 22:35:42 +0530

    Adding review input component

---
+++ src/app/components/base-components/review-input/review-input.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-input',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './review-input.component.html',
+  styleUrl: './review-input.component.css'
+})
+export class ReviewInputComponent {
+
+}
--- src/app/components/pages/product-details-page/product-details-page.component.ts
+++ src/app/components/pages/product-details-page/product-details-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommentComponent } from '../../base-components/comment/comment.component';
 import { CommonModule } from '@angular/common';
+import { ReviewInputComponent } from '../../base-components/review-input/review-input.component';
 
 @Component({
   selector: 'app-product-details-page',
   standalone: true,
-  imports: [CommentComponent, CommonModule],
+  imports: [CommentComponent, CommonModule, ReviewInputComponent],
   templateUrl: './product-details-page.component.html',
   styleUrl: './product-details-page.component.css'
 })
