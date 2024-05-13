5fd215797072dc41746275528b3afec478bb3797
Ankan-Basu <an.basu.kan@gmail.com>
2024-05-13 21:34:37 +0530

    Created navbar and product list item

---
+++ src/app/components/base-components/product-list-item/product-list-item.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-list-item.component.html',
+  styleUrl: './product-list-item.component.css'
+})
+export class ProductListItemComponent {
+
+}
