fb044c265443ec186d812c4635d0bdc5eb7635cb
Ankan-Basu <an.basu.kan@gmail.com>
2024-05-13 20:12:20 +0530

    Project init file str

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#7469B6',
+        'primary2': '#6358a6',
+        'secondary': '#AD88C6',
+        'secondary2': '#E1AFD1',
+        'tertiary': '#FFE6E6'
+      },
+    },
   },
   plugins: [],
 }
