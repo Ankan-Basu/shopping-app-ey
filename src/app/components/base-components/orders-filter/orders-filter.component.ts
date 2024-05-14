f82ba2802e4a590ccacb51f94b21d283459a6456
Ankan-Basu <an.basu.kan@gmail.com>
2024-05-14 14:06:10 +0530

    Making orders list page

---
+++ src/app/components/base-components/orders-filter/orders-filter.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-filter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './orders-filter.component.html',
+  styleUrl: './orders-filter.component.css'
+})
+export class OrdersFilterComponent {
+
+}
---
+++ src/app/components/base-components/orders-item/orders-item.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './orders-item.component.html',
+  styleUrl: './orders-item.component.css'
+})
+export class OrdersItemComponent {
+
+}
