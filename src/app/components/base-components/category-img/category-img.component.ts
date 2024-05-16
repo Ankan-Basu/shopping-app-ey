6e1c9523c222f344f60c77cfba6ebd8293d7e115
Ankan-Basu <an.basu.kan@gmail.com>
2024-05-16 16:27:34 +0530

    Making component cards

--- src/app/components/base-components/category-img/category-img.component.ts
+++ src/app/components/base-components/category-img/category-img.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-category-img',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './category-img.component.html',
   styleUrl: './category-img.component.css'
 })
 export class CategoryImgComponent {
   @Input() imgUrl: string = '';
   @Input() catName: string = '';
+  @Input() redirLink: string = '';
 
 }
