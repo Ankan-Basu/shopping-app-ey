ffde8c0903ef0f46ec91af20ba7178e6ebad26e2
Ankan-Basu <an.basu.kan@gmail.com>
2024-05-17 12:55:35 +0530

    making profile page

--- src/app/components/pages/profile-page/profile-page.component.ts
+++ src/app/components/pages/profile-page/profile-page.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { SidebarUserComponent } from '../../base-components/sidebar-user/sidebar-user.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [],
+  imports: [SidebarUserComponent, CommonModule],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.css'
 })
 export class ProfilePageComponent {
-
+  appName = 'ShopEy';
+  email = 'freshbrother@mail.com';
+  phone = '8876655423';
+  address = '19 Wet Market, Wuhan, Huebei - 500021, China';
+  balance = 2210.56;
 }
